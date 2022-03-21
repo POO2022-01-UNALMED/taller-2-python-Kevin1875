750b65c85ddc930e2575f0583093afc29bba94ef
Kevin1875 <94397214+Kevin1875@users.noreply.github.com>
2022-03-21 14:47:23 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Asiento:
 
 class Auto:
   cantidadCreados=0
-  def __init__ (self, modelo, marca, asientos, precio, motor,  registro):
+  def __init__ (self, modelo, precio, asientos, marca, motor,  registro):
     self.modelo=modelo
     self.asientos=asientos
     self.registro=registro
@@ -23,28 +23,33 @@ class Auto:
 
   def cantidadAsientos(self):
     c=0
+    l=[]
     prueba=Asiento("verde", 22,0)
    
     for i in self.asientos:
-      
+      if i != None:
+        l.append(i)
       if type(prueba)==type(i):
         c+=1
-    return("Numeros de asientos", + c)
+    return(c)
 
 
   def verificarIntegridad(self):
     l=[]
     r=0
-    a=self.asientos[0]
+    c=0
     for i in self.asientos:
-      l.append(i.registro)
-      if l.count(a.registro)==len(l):
-        r=a.registro
-        
-    if self.motor.registro==self.registro and self.registro == r:
-      return("Auto original")
-    else:
-      return("Las piezas no son originales")
+      if i != None:
+        c+=1
+        if i.registro==self.registro and i.registro==self.motor.registro:
+          l.append(i.registro)
+          if len(l)==c:
+            return("Auto original")
+          else:
+            return("Las piezas no son originales")
+          
+    
+    
 
 class Motor:
   def __init__ (self, numerosCilindros, tipo, registro):
@@ -62,23 +67,16 @@ class Motor:
   
 
 if __name__ == "__main__":
-  asiento1= Asiento("rojo", 36, 2)
-  asiento2= Asiento("rojo", 36, 2)
-  asiento3=Motor("rojo", 36, 4)
-  motor1=Motor(1, "gasolina", 2)
-  motor2=Motor(1, "gasolina", 2)
-  a1= Auto("a", "bw", [asiento1,asiento2, asiento3], 32, motor1, 2)
-  a2= Auto("a", "bw", [asiento1,asiento2], 32, motor2, 2)
-  motor1.asignarTipo("electrico")
-  print(motor1.tipo)
-  print(a1.cantidadAsientos())
+  a = Auto("model 3", 33000, list(),"tesla", Motor(4, "electrico", 142), 341)
+  a.asientos = [Asiento("blanco", 5000, 435),None, None, Asiento("blanco", 5000, 435), None]
+  
+  
+  a1 = Auto("model 3", 33000, [Asiento("blanco", 5000, 32),None, None, Asiento("blanco", 5000, 32), None], "tesla", Motor(4, "electrico", 32), 32)
   print(a1.verificarIntegridad())
-  asiento1.cambiarColor("rosa")
-  print(asiento1.color)
-  print(a1.cantidadCreados)
-  
-  
-  
+  print(a1.cantidadAsientos())
+  #print(a1.motor)
+  a2 = Auto("model 3", 33000, [Asiento("blanco", 5000, 40),None, None, Asiento("blanco", 5000, 32), None], "tesla", Motor(4, "electrico", 32), 32)
+  print(a2.verificarIntegridad())
 
   
     
